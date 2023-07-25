8644ccce6f30bbd63f22bc408cccea6a04084d7e
AlexanderSoriano21 <renealexander727@gmail.com>
2023-07-24 19:41:56 -0600

    :fire: feat(bootcamps): Bootcamps con estilos Bootstrap

--- views/bootcampView.php
+++ views/bootcampView.php
@@ -4,18 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kodigo App</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Bienvenido a la Kodigo App</h1>
-    <h2>Todos los Bootcamps: </h2>
-    <!-- -->
-    <ul>
-        <?php foreach($bootcamps as $bootcamp): ?>
-            <li>
-                <h2><?php echo $bootcamp['title']?></h2>
-                <p><?php echo $bootcamp['description']?></p>
-            </li>
-        <?php endforeach; ?>
-    </ul>
+    <h1 class="text-center">Bienvenido a la Kodigo App</h1>
+    <h2 class="text-center">Todos los Bootcamps: </h2>
+
+    <div class="container">
+        <div class="row">
+            <!-- Lista de Bootcamps -->
+            <?php foreach($bootcamps as $bootcamp): ?>
+                <div class="col-sm-3">
+                    <div class="card" style="width: 18rem;">
+                        <!-- Imagenes de Bootcamp -->
+                        <img src="https://www.codigoonclick.com/wp-content/uploads/2018/02/mejores-lenguajes-de-programacion-2018.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $bootcamp['title']?></h5>
+                            <p class="card-text"><?php echo $bootcamp['description']?></p>
+                            <a href="#" class="btn btn-primary">Leer más</a>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
 </body>
 </html>
